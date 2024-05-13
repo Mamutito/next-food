2aa435e3e762547de9d4a603f536887c8d61356d
Mamutito <aithor123@gmail.com>
2024-05-13 14:46:39 +0100

    feat: get meals from db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,9 @@
 import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import Link from "next/link";
-function MealsPage() {
+import { getMeals } from "@/lib/meals";
+async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +19,7 @@ function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
