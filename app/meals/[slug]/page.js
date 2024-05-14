d56991407b217e1ce814849dc50f5caebadbcf8f
Mamutito <aithor123@gmail.com>
2024-05-14 18:27:40 +0100

    feat: meal details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,36 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
 function MealDetailPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+  const { title, summary, creator, creator_email, image } = meal;
+  const instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
     <>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meal Detail Page</h1>
-      <p>{params.slug}</p>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={image} alt={summary} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{title}</h1>
+          <p className={classes.creator}>
+            <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={classes.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: instructions }}
+        ></p>
+      </main>
     </>
   );
 }
